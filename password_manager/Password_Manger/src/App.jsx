c6f1f29698dd20f27c4e39dd7674657c5fafc72a
atishay <118368229+Atishay3012@users.noreply.github.com>
2024-05-03 15:21:56 +0530

    Update App.jsx

--- password_manager/Password_Manger/src/App.jsx
+++ password_manager/Password_Manger/src/App.jsx
@@ -1,21 +1,28 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './components/Navbar'
-import Manager from './components/Manager'
-import Footer from './components/Footer'
+import React from 'react';
+import './App.css';
+import Welcome from './components/welcome'; 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/about';
+import ContactUs from './components/contact';
+// import PasswordList from './components/PasswordList';
 
 function App() {
-
-  return (
-    <>
-      <Navbar />
-      <div className="bg-yellow-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
-      {/* <div className="bg-black"> */}
-        <Manager />
-      </div>
-      <Footer />
-    </>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Welcome />} />
+                {/* Render Navbar only for routes other than Welcome */}
+                <Route path='/*' element={<Navbar />} />
+                <Route path='/home' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/contact' element={<ContactUs />} />
+                {/* <Route path='/passwordlist' element={<PasswordList />} /> */}
+                
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
