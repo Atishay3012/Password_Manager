f6fb435026f5dc927c8ac0a8d1bcd3aa40b346e6
atishay <118368229+Atishay3012@users.noreply.github.com>
2025-03-08 03:38:33 +0530

    Update welcome.jsx

--- password_manager/Password_Manger/src/components/welcome.jsx
+++ password_manager/Password_Manger/src/components/welcome.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-// import "./welcome.css"
 import axios from 'axios'
 import { Link ,useNavigate} from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
