5699cc84cd2988a74d54a8592200f5ee0969d8fb
atishay <118368229+Atishay3012@users.noreply.github.com>
2025-03-08 18:37:31 +0530

    Update vite.config.js

--- password_manager/Password_Manger/vite.config.js
+++ password_manager/Password_Manger/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
+    base: "/",
     plugins: [react()],  // ✅ Make sure this is inside an object
     build: {
       outDir: "dist",   // ✅ Ensure build output is set correctly
